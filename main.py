eb3f1d10de29a3fa6a1589d6c19eeeeae38aa2e3
snowjtSD <jordantreysnow@gmail.com>
2025-03-01 06:10:29 +0000

    changes to code to count number of words

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 def get_book_text(file_text):
     with open(file_text) as f:
         file_text = f.read()
-        print(file_text)
         return file_text
 
-def main():
-    get_book_text("books/frankenstein.txt")
+def num_words(file_text):
+    word_list = file_text.split()
+    num_words = 0
+    for i in word_list:
+        num_words += 1
+    print(f"{num_words} words found in the document")
 
+
+def main():
+    file_contents = get_book_text("books/frankenstein.txt")
+    num_words(file_contents)
 
 main()
