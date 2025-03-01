b59954dfb102f563a8a29b174b513774e87ceeb7
snowjtSD <jordantreysnow@gmail.com>
2025-03-01 05:21:42 +0000

    functionaloty to read and print file contents

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("greetings boots")
+def get_book_text(file_text):
+    with open(file_text) as f:
+        file_text = f.read()
+        print(file_text)
+        return file_text
+
+def main():
+    get_book_text("books/frankenstein.txt")
+
+
+main()
