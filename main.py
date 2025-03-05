741a3d5a6642e18947e3cc2ce9eb316ba276fb2e
snowjtSD <jordantreysnow@gmail.com>
2025-03-05 17:54:57 +0000

    changes to main and stats to sort and iterate through data

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 from stats import num_words
 from stats import letter_count
+from stats import sort_dict
 
 def get_book_text(file_text):
     with open(file_text) as f:
         file_text = f.read()
         return file_text
 
+def sort_on(dict):
+    return dict[i]
+
 def main():
     file_contents = get_book_text("books/frankenstein.txt")
-    num_words(file_contents)
-    letter_count(file_contents)
-
+    words = num_words(file_contents)
+    letters = letter_count(file_contents)
+    sorted_list_dict = sort_dict(letters)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Found {words} total words")
+    print("--------- Character Count -------")
+    for element in sorted_list_dict:
+        print(f"{element['name']}: {element['num']}")
+    print("============= END ===============")
 main()
--- stats.py
+++ stats.py
@@ -3,17 +3,26 @@ def num_words(file_text):
     num_words = 0
     for i in word_list:
         num_words += 1
-    return print(f"{num_words} words found in the document")
-
+    return num_words
 
 def letter_count(file_text):
     letter_dict = {}
     word_list = file_text.split()
     for word in word_list:
         word = word.lower()
-        for i in word:
-            if i in letter_dict:
-                letter_dict[i] += 1
+        for char in word:
+            if char in letter_dict:
+                letter_dict[char] += 1
             else:
-                letter_dict[i] = 1
-    print(letter_dict)
+                letter_dict[char] = 1
+    return letter_dict
+
+def sort_on(dict):
+    return dict['num']
+
+def sort_dict(some_dict):
+    sorted_list = []
+    for i in some_dict:
+        sorted_list.append(dict(name=i, num=some_dict[i]))
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
